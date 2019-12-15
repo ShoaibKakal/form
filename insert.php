8f373b6b817e477bd3152c9ca03ccc950717d053
Shoaib Khalid <56650286+Shoaibpython@users.noreply.github.com>
2019-12-15 20:53:29 +0500

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,31 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password ="";
+$database ="customer";
+
+$connect = mysqli_connect($host,$user,$password,$database);
+if($connect){
+    echo" databas has been connected";
+}else{
+    echo "Try again";
+}
+
+$fname = $_POST['1'];
+$lname = $_POST['2'];
+$cnic = $_POST['3'];
+$age = $_POST['6'];
+$weight = $_POST['7'];
+$joining = $_POST['8'];
+
+
+$query="insert into Registration(First_Name,Last_Name,CNIC,Age,Weight,Joining) values('$fname','$lname','$cnic','$age','$weight','$joining')";
+$transport=mysqli_query($connect,$query);
+if($transport){
+    echo " data updated";
+}else{
+    echo "sorry something went wrong";
+}
+
+?>
